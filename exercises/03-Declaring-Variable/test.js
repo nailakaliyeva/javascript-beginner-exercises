e2d9837ef90a59dd19d23f819843596ea195bcfc
Naila <nailakaliyeva@gmail.com>
2019-09-06 22:16:11 +0000

    declaring vars testing code is not finished. whatever needs to be solved is commented out

--- exercises/03-Declaring-Variable/test.js
+++ exercises/03-Declaring-Variable/test.js
@@ -1,7 +1,10 @@
 const color = require("./app.js")
+//Since I don't know what a developer going to call
+//his variable I need to figure out a way
+//to let him call it whatever he wants
 test("declare and console.log a variable", ()=>{
-    //expect(console.log).toHaveBeenCalledTimes(1)
-    //expect(color).toBeDefined()
-    expect(color).toBe("Yellow")
+    expect(color).toBeDefined();
+    expect(color).toBe("Yellow");
+    //expect(console.log).toHaveBeenCalledWith("Yellow");
 
 })
