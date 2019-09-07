8daf43aa852db6a7dfd7e436de67f62585d0175d
Naila <nailakaliyeva@gmail.com>
2019-09-07 20:05:57 +0000

    I just found the code to test whats in console that is working. Still dont understand how it works. Yet

--- exercises/03-Declaring-Variable/test.js
+++ exercises/03-Declaring-Variable/test.js
@@ -1,10 +1,22 @@
-const color = require("./app.js")
+// const color = require("./app.js")
 //Since I don't know what a developer going to call
 //his variable I need to figure out a way
 //to let him call it whatever he wants
-test("declare and console.log a variable", ()=>{
-    expect(color).toBeDefined();
-    expect(color).toBe("Yellow");
+let outputData = "";
+storeLog = (inputs) => (outputData = outputData + inputs);
+test("declare and console.log a variable", () => {
+  console["log"] = jest.fn(storeLog);
+  require("./app.js");
+  expect(outputData).toBe("Yellow");
+});
+//when I copy line 9 from 02-Hello-world
+//line 11 it at least counts if it was called 0
+//or 1 time saying receved # of calls(line 13)
+//global.console.log = console.log = jest.fn((text) => _buffer += text + "\n");
+// test("declare and console.log a variable", ()=>{
+//     expect(color).toBeDefined();
+//     expect(color).toBe("Yellow");
+    //expect(console.log).toHaveBeenCalledTimes(1);
     //expect(console.log).toHaveBeenCalledWith("Yellow");
 
-})
+// })
