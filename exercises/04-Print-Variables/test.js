1c2e3839579ebdc6df4f4bda6cca971dbdc51958
Naila <nailakaliyeva@gmail.com>
2019-09-07 21:47:15 +0000

    I think I figured out how mock console.log works in 04-Print-Variables test.js

--- exercises/04-Print-Variables/test.js
+++ exercises/04-Print-Variables/test.js
@@ -1,8 +1,35 @@
-const color = require("./app.js")
-test("print  a variable", ()=>{
+let _buffer = "";
 
+console.log = jest.fn((text) => _buffer += text + "\n");
+
+test('console.log() function should be called with Hello World',  () =>{
+    const color = require("./app.js");
+    expect(console.log).toHaveBeenCalledWith("red");
+    expect(console.log.mock.calls.length).toBe(1);
     expect(color).toBeDefined();
-    expect(color).toBe("red");
-    //expect(console.log.toHaveBeenCalledWith("red");
+    })
 
-})
+
+
+
+
+
+//this is the lamest way to do it bc it doesnt check
+//if console.log was used
+// test("print  a variable", ()=>{
+
+//     expect(color).toBeDefined();
+//     expect(color).toBe("red");
+//     //expect(console.log.toHaveBeenCalledWith("red");
+
+// })
+
+// let text = "";//Is that how I do it?
+// storeLog = (inputs) => (text = text + inputs);
+// test("declare and console.log a variable", () => {
+//   console["log"] = jest.fn(storeLog);
+//   require("./app.js");
+//   expect(text).toBe("red");
+//   expect(text).toBeDefined();//can i check if it was
+//   //defined specifically as a variable?
+// });
