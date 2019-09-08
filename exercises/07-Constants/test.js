8ffe3313a8aafdb4fecc9f05941be676dd4ab59f
Naila <nailakaliyeva@gmail.com>
2019-09-08 00:11:11 +0000

    figured out how to check if its a var/let or a const, added function to 07-Constants test.js

--- exercises/07-Constants/test.js
+++ exercises/07-Constants/test.js
@@ -1,6 +1,20 @@
-const VERSION = require("./app.js")
 test("Substitute const for let or var", ()=>{
+const VERSION = require("./app.js")
+    function isConst(VERSION){
+        try {
+        VERSION = "test";
+        }
+        catch(err){}
+        if (VERSION==="test"){
+            return "You've successfully changed the type of variable to const"
+        }
+        else return "Cannot change const"}
+        //the reason I created this function is because I want to see if VERSION
+        //will be reassigned with a different value
+        //if it doesn't get reassigned then it means the student failed to change const
+        //for var
     expect(VERSION).toBeDefined();
     expect(typeof VERSION).toBe("string");
-    //how do I specify that this should be var/let and not a const??
+    expect(isConst(VERSION)).toBe("You've successfully changed the type of variable to const");
+    //how do I specify that this should be var/let and not a const easier way??
 })
